38fad587a735644adfbe184d8fd2b6ef7c1bc0e0
nit <akshay.gurnule@gmail.com>
2018-07-31 12:11:29 +0530

    email

--- app/src/main/java/com/akshay/telephony/EmailActivity.kt
+++ app/src/main/java/com/akshay/telephony/EmailActivity.kt
@@ -3,8 +3,6 @@ package com.akshay.telephony
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.app.AlertDialog
-import android.support.v7.widget.AlertDialogLayout
 import kotlinx.android.synthetic.main.activity_email.*
 
 class EmailActivity : AppCompatActivity() {
@@ -14,16 +12,18 @@ class EmailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_email)
 
         b1.setOnClickListener {
-            AlertDialogLayout(this, )
+
+
         }
 
         send.setOnClickListener {
             val i = Intent()
+            i.action = Intent.ACTION_SEND
             i.putExtra(Intent.EXTRA_EMAIL,et1.text.toString())
             i.putExtra(Intent.EXTRA_SUBJECT,et2.text.toString())
             i.putExtra(Intent.EXTRA_TEXT,et3.text.toString())
-            i.putExtra(Intent.EXTRA_STREAM,)
-            i.type = "message/rfc822"
+          //  i.putExtra(Intent.EXTRA_STREAM,et1.text.toString())
+         //   i.type = "message/rfc822"
             startActivity(i)
         }
     }
